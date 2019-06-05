b0e216ea8f84be1954722f3b32722a6ba78e76fe
Khaya R. Dillon <yahyal0v3@yahoo.com>
2019-06-04 22:13:14 -0400

    Complete exercise two

--- app.js
+++ app.js
@@ -19,3 +19,26 @@ const soccerResponseP = document.createElement("p")
 soccerResponseP.appendChild(document.createTextNode(likesSoccerBoolean))
 soccerDiv.append(soccerResponseP)
 
+
+/* 
+Exercise Two 
+ 
+Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will
+check if the current number is odd or even, and display a message to the screen.
+
+Sample Output :
+"0 is even"
+"1 is odd"
+"2 is even"
+... 
+*/
+
+for (var i = 0; i <= 15; i++) {
+	if (i % 2 === 0) {
+		console.log(`${i} is even`)
+	} else {
+		console.log(`${i} is odd`)
+	}
+}
+
+
